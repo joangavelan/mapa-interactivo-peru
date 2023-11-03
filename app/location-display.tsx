600ba6c6afc642d9ac48615ee671884c3eb3a2b3
joangavelan <joangavelan@gmail.com>
2023-11-02 22:45:31 -0500

    format location text

--- app/location-display.tsx
+++ app/location-display.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Territory } from '@/types'
 import { BiWorld } from 'react-icons/bi'
+import { capitalizeWords } from './utils'
 
 export const LocationDisplay = ({ enteredTerritories }: { enteredTerritories: Territory[] }) => {
   const location = enteredTerritories
@@ -11,7 +12,9 @@ export const LocationDisplay = ({ enteredTerritories }: { enteredTerritories: Te
   return (
     <div className='absolute left-1/2 -translate-x-1/2 bottom-0 bg-white px-4 py-2.5 rounded-tl-md rounded-tr-md border shadow-sm flex items-center gap-2'>
       <BiWorld className='text-lg text-gray-600' />
-      <p className='text-sm text-gray-600'>{location || 'Seleciona un territorio'}</p>
+      <p className='text-sm text-gray-600'>
+        {capitalizeWords(location) || 'Seleciona un territorio'}
+      </p>
     </div>
   )
 }
